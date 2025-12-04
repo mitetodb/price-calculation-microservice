c092f4f729442c8c4c82bb669b0e1d1ee1549940
mitetodb <65173088+mitetodb@users.noreply.github.com>
2025-12-04 19:04:24 +0200

    fix: implement feign connection in main application and fix the connection in rest api

--- src/main/java/com/controller/PriceCalculationController.java
+++ src/main/java/com/controller/PriceCalculationController.java
@@ -3,20 +3,35 @@ package com.controller;
 import com.model.PriceCalculationRequest;
 import com.model.PriceCalculationResponse;
 import com.service.PriceCalculationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/price")
+@RequestMapping("/pricing")
+@RequiredArgsConstructor
 public class PriceCalculationController {
 
     private final PriceCalculationService service;
 
-    public PriceCalculationController(PriceCalculationService service) {
-        this.service = service;
-    }
+    @Value("${pricing.demo-mode:false}")
+    private boolean demoMode;
 
     @PostMapping("/calculate")
-    public PriceCalculationResponse calculate(@RequestBody PriceCalculationRequest request) {
+    public ResponseEntity<PriceCalculationResponse> calculate(@RequestBody PriceCalculationRequest request) {
+
+        if (demoMode) {
+            return ResponseEntity.ok(generateDemoPrice(request));
+        }
+
+        // response
+        PriceCalculationResponse response = service.calculate(request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    private PriceCalculationResponse generateDemoPrice(PriceCalculationRequest request) {
         return service.calculate(request);
     }
 }
